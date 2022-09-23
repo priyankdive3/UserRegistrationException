6dca294edab56be0c053a17ec4f38273d38203e3
Priyanka <priyanka.divekar1100@gmail.com>
2022-09-23 22:03:53 +0530

    [Priyanka]Last Name

---
+++ UserException/UserException/UC2Last.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserException
+{
+    public class UC2Last
+    {
+        public static void ValidateLastName()
+        {
+            try
+            {
+                Console.WriteLine("Enter Last Name");
+                String lastName = Console.ReadLine();
+                string pattern = "^[A-Z]{1}[a-z]{2,}$";
+                if (Regex.IsMatch(lastName, pattern))
+                {
+                    Console.WriteLine("First Name : " + lastName);
+                }
+                else
+                {
+                    throw (new CustomException("Last name should be first letter capital and more then 2 characters"));
+                }
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
